271ef3eedd87ab893809daee87d9e9ed3a5b6fd4
Rafa Macbook <rafaellemontoya9@gmail.com>
2019-10-28 19:47:25 -0600

    Initial Commit

---
+++ K-Forum 2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  K-Forum 2020
+//
+//  Created by Rafael Montoya on 10/28/19.
+//  Copyright © 2019 Administra Servicios Integrales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
